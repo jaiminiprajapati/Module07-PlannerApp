9d9db8c72887690050dbe6d2686d123b8d8cceb5
jaiminiprajapati <jaiminihp397@gmail.com>
2024-01-23 09:47:36 +0000

    Timeblock generated in script file

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     // Display current day at the top of the calendar
     $("#currentDay").text(dayjs().format("MMMM D, YYYY"));
+
+    // Generate timeblocks
+    for (let i = 9; i <=17; i++){
+        let row = $("<div>");
+        let time = $("<div>");
+        let description = $("<textarea>");
+        let saveBtn = $("<button>");
+    }
 });
